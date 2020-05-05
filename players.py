7bc4e89657cbf7b26daa553ed4c1cbeab67507bf
Xe-Xo <50796415+Xe-Xo@users.noreply.github.com>
2020-05-06 06:53:58 +1000

    create players class

---
+++ players.py
@@ -0,0 +1,22 @@
+from resources import ResourceBunch
+
+class Player():
+    def __init__(self,playerindex):
+        self.resources = ResourceBunch(0,0,0,0,0)
+        self.playerindex = playerindex
+
+    def add_resouces(self,resourcebunch):
+        self.resources = self.resources + resourcebunch
+
+    def discard_resouces(self,resourcebunch):
+        self.resources = self.resources.discard(resourcebunch)
+
+class Human(Player):
+
+    def __init__(self,playerindex):
+        super().__init__(playerindex)
+
+class Bot(Player):
+
+    def __init__(self,playerindex):
+        super().__init__(playerindex)
