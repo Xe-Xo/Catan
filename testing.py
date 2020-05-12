f31a60b24490a4188cb88c6aaf0bc4e7066273f7
Xe-Xo <50796415+Xe-Xo@users.noreply.github.com>
2020-05-12 16:32:55 +1000

    testing coord rounding
    testing for coord rounding

--- testing.py
+++ testing.py
@@ -1,3 +1,17 @@
-from game import GAME_GLOBALS
+from grid import Coords
+import math
 
-print(GAME_GLOBALS.ColorRange(GAME_GLOBALS.KHAKI,GAME_GLOBALS.MOCCASIN,3))
+#testpos = Coords(-0.47290592754970234, -0.06876073911696429, 0.5416666666666666).round(1)
+#print(testpos)
+#testneg = Coords(-0.47290592754970234, -0.06876073911696429, 0.5416666666666666).round(-1)
+#print(testneg)
+
+#should return -1,-1,1 & 0,-1,2
+
+test1 = Coords(0.45, -1.495, 1.04)
+test2 = Coords(0.374,-1.574,1.2)
+
+print(test1.round(1))
+print(test2.round(1))
+
+
